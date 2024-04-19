a5de13dc0ad46bfb491d127ceab92267f730c11a
BrayanRunt <brayan.burgos@runt.com.co>
2024-04-19 15:04:20 -0500

    evaluacion

--- cypress/e2e/2-selectores_clase/selectores_clase.cy.js
+++ cypress/e2e/2-selectores_clase/selectores_clase.cy.js
@@ -5,6 +5,24 @@ describe("Test selector Clase", () => {
         cy.clearAllSessionStorage()
         cy.clearAllCookies()
         cy.visit("https://admin-demo.nopcommerce.com/login")
-    })
 
+        cy.get("#Email").clear();
+        cy.get('#Email').type('admin@yourstore.com') 
+        cy.get('button.login-button').click();
+
+    })
+    it("Productos", () => {
+        cy.contains('Catalog').click()
+        cy.contains('Products').click({force: true})
+        cy.wait(5000);
+        cy.get('#products-grid tbody tr:nth-child(1)').find('a').click()
+        cy.wait(5000);
+        cy.get('[name="save"]').click({force: true})
+        cy.get('#products-grid tbody tr:nth-child(2)').find('a').click()
+        cy.wait(5000);
+        cy.get('[name="save"]').click({force: true})
+        cy.get('#products-grid tbody tr:nth-child(3)').find('a').click()
+        cy.wait(5000);
+        cy.get('[name="save"]').click({force: true})
+    })
 })
